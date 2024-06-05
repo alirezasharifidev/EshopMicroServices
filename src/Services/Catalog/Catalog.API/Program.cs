c2f605cabba1b13481aeb7765e978e6e21798e58
Alireza Sharifi <alirezasharifi.dev@gmail.com>
2024-06-05 17:21:10 +0330

    carter

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,9 +2,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container. (DI)
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+
+});
 
 var app = builder.Build();
 
 // Configure the http request pipline. MapGet...
+app.MapCarter();
 
 app.Run();
